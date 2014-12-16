517c417d03a969c820293bcec771625eeff3edf9
ChrisMcKenzie <chris@kbsurfer.com>
2014-12-15 19:17:35 -0800

    fixin shit

--- zsh/zsh-aliases.zsh
+++ zsh/zsh-aliases.zsh
@@ -16,3 +16,13 @@ alias -g G='| grep' # now you can do: ls foo G something
 # to find all files containing 'foo' in the name
 function fn() { ls **/*$1* }
 
+# Git Aliases
+hub_path=$(which hub)
+echo $hub_path
+if (( $+commands[hub] )) then
+    alias git=$hub_path
+fi
+
+pj() { cd $PROJECTS/$1; }
+compctl -W $PROJECTS -/ pj
+
